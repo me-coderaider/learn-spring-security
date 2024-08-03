9787e83407784828373015c2ced17713357bcfa3
me-coderaider <kashyappankajnsp@gmail.com>
2024-08-03 10:01:51 +0530

    Storing UserDetails inMemory

---
+++ src/main/java/rtg/learning/spring_security/basic/BasicAuthSecurityConfiguration.java
@@ -0,0 +1,91 @@
+package rtg.learning.spring_security.basic;
+
+import static org.springframework.security.config.Customizer.withDefaults;
+
+import javax.sql.DataSource;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseBuilder;
+import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseType;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.jdbc.JdbcDaoImpl;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.provisioning.JdbcUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+public class BasicAuthSecurityConfiguration {
+	@Bean
+	SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+//		http.authorizeHttpRequests((requests) -> requests.anyRequest().authenticated());
+//		http.formLogin(withDefaults());
+//		http.httpBasic(withDefaults());
+//		return http.build();
+		
+		http.authorizeHttpRequests(auth ->{
+			auth.anyRequest().authenticated();
+		});
+		http.sessionManagement(
+				session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+				);
+//		http.formLogin();
+//		http.httpBasic();
+//		http.csrf().disable();
+		
+		http.httpBasic(withDefaults());
+		http.csrf(AbstractHttpConfigurer::disable);
+		
+		http.headers().frameOptions().sameOrigin();
+		
+		return http.build();
+		
+	}
+	
+	@Bean
+	public UserDetailsService userDetailService() {
+		var user = User.withUsername("coderaider")
+			.password("{noop}dummy")
+			.roles("USER")
+			.build();
+		
+		var admin = User.withUsername("admin")
+				.password("{noop}dummy")
+				.roles("ADMIN")
+				.build();
+		
+		return new InMemoryUserDetailsManager(user, admin);
+	}
+	
+//	@Bean
+//	public DataSource dataSource() {
+//		return new EmbeddedDatabaseBuilder()
+//				.setType(EmbeddedDatabaseType.H2)
+//				.addScript(JdbcDaoImpl.DEFAULT_USER_SCHEMA_DDL_LOCATION)
+//				.build();
+//	}
+//	
+//	@Bean
+//	public UserDetailsService userDetailService(DataSource dataSource) {
+//		var user = User.withUsername("coderaider")
+//			.password("{noop}dummy")
+//			.roles("USER")
+//			.build();
+//		
+//		var admin = User.withUsername("admin")
+//				.password("{noop}dummy")
+//				.roles("ADMIN")
+//				.build();
+//		
+//		var jdbcUserDetailsManager=new JdbcUserDetailsManager(dataSource);
+//		jdbcUserDetailsManager.createUser(user);
+//		jdbcUserDetailsManager.createUser(admin);
+//		
+//		
+//		return jdbcUserDetailsManager;
+//	}
+}
