2ef97b7fc6a6f8828f6afaa54c5478ffa3df9909
me-coderaider <kashyappankajnsp@gmail.com>
2024-08-03 14:38:02 +0530

    1. Create Key Pair

--- src/main/java/rtg/learning/spring_security/jwt/JwtSecurityConfiguration.java
+++ src/main/java/rtg/learning/spring_security/jwt/JwtSecurityConfiguration.java
@@ -1,6 +1,9 @@
-package rtg.learning.spring_security.basic;
+package rtg.learning.spring_security.jwt;
 
 import static org.springframework.security.config.Customizer.withDefaults;
+
+import java.security.KeyPair;
+import java.security.KeyPairGenerator;
 
 import javax.sql.DataSource;
 
@@ -10,6 +13,7 @@ import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseBuilder;
 import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseType;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.annotation.web.configurers.oauth2.server.resource.OAuth2ResourceServerConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetailsService;
@@ -18,83 +22,79 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.provisioning.JdbcUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
-//@Configuration
-public class BasicAuthSecurityConfiguration {
+@Configuration
+public class JwtSecurityConfiguration {
 	@Bean
 	SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
 //		http.authorizeHttpRequests((requests) -> requests.anyRequest().authenticated());
 //		http.formLogin(withDefaults());
 //		http.httpBasic(withDefaults());
 //		return http.build();
-		
-		http.authorizeHttpRequests(auth ->{
+
+		http.authorizeHttpRequests(auth -> {
 			auth.anyRequest().authenticated();
 		});
-		http.sessionManagement(
-				session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS)
-				);
-//		http.formLogin();
+		http.sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS));
+		http.formLogin();
 //		http.httpBasic();
 //		http.csrf().disable();
-		
+
 		http.httpBasic(withDefaults());
 		http.csrf(AbstractHttpConfigurer::disable);
-		
+
 		http.headers().frameOptions().sameOrigin();
-		
+
+		// HERE, WE'RE CONFIGURING THE OAUTH2 RESOURCE SERVER.
+//		THIS RESOURCE SERVER, WHEN IT RECEIVES A JWT TOKEN, IT NEEDS TO DECODE IT
+//		AND TO DECODE, IT NEEDS A DECODER I.E JWTDECODER
+		http.oauth2ResourceServer(OAuth2ResourceServerConfigurer::jwt);
+
 		return http.build();
-		
+
 	}
-	
-//	@Bean
-//	public UserDetailsService userDetailService() {
-//		var user = User.withUsername("coderaider")
-//			.password("{noop}dummy")
-//			.roles("USER")
-//			.build();
-//		
-//		var admin = User.withUsername("admin")
-//				.password("{noop}dummy")
-//				.roles("ADMIN")
-//				.build();
-//		
-//		return new InMemoryUserDetailsManager(user, admin);
-//	}
-	
+
 	@Bean
 	public DataSource dataSource() {
-		return new EmbeddedDatabaseBuilder()
-				.setType(EmbeddedDatabaseType.H2)
-				.addScript(JdbcDaoImpl.DEFAULT_USER_SCHEMA_DDL_LOCATION)
-				.build();
+		return new EmbeddedDatabaseBuilder().setType(EmbeddedDatabaseType.H2)
+				.addScript(JdbcDaoImpl.DEFAULT_USER_SCHEMA_DDL_LOCATION).build();
 	}
-	
+
 	@Bean
 	public UserDetailsService userDetailService(DataSource dataSource) {
 		var user = User.withUsername("coderaider")
 //			.password("{noop}dummy")
-			.password("dummy")
-			.passwordEncoder(str -> passwordEncoder().encode(str)) 
-			.roles("USER")
-			.build();
-		
+				.password("dummy").passwordEncoder(str -> passwordEncoder().encode(str)).roles("USER").build();
+
 		var admin = User.withUsername("admin")
 //				.password("{noop}dummy")
-				.password("dummy")
-				.passwordEncoder(str -> passwordEncoder().encode(str)) 
-				.roles("ADMIN")
-				.build();
-		
-		var jdbcUserDetailsManager=new JdbcUserDetailsManager(dataSource);
+				.password("dummy").passwordEncoder(str -> passwordEncoder().encode(str)).roles("ADMIN").build();
+
+		var jdbcUserDetailsManager = new JdbcUserDetailsManager(dataSource);
 		jdbcUserDetailsManager.createUser(user);
 		jdbcUserDetailsManager.createUser(admin);
-		
-		
+
 		return jdbcUserDetailsManager;
 	}
-	
+
 	@Bean
 	public BCryptPasswordEncoder passwordEncoder() {
 		return new BCryptPasswordEncoder();
 	}
+
+	@Bean
+	public KeyPair keyPair() {
+		try {
+			var keyPairGenerator = KeyPairGenerator.getInstance("RSA");
+			keyPairGenerator.initialize(2048);
+			return keyPairGenerator.generateKeyPair();
+		} catch (Exception ex) {
+			throw new RuntimeException(ex);
+		}
+
+	}
+
+//	@Bean
+//	public JwtDecoder jwtDecoder() {
+//		return decoder;
+//	}
 }
